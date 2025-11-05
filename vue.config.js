a9777b95702b626ea5745922f92c93cc4a48db7f
Alex Krai <krai_demon@mail.ru>
2025-11-06 00:54:48 +0300

    Fix publicPath for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/calendar/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/test-task/' : '/'
 })
